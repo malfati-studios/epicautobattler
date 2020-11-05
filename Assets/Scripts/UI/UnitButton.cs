4d9f1c11ac7f1b9aed2db6394e2c86ecac7a3153
malfati-studios <malfati.studios@gmail.com>
2020-11-05 19:52:41 -0300

    buttons

---
+++ Assets/Scripts/UI/UnitButton.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class UnitButton : MonoBehaviour
+    {
+        public Action<GameObject> buttonListeners;
+        [SerializeField] private GameObject unitPrefab;
+
+        private Image unitImage;
+        private TextMeshProUGUI unitCountTxt;
+        private int unitCount;
+    
+    
+        public void SetUnitCount(int count)
+        {
+            unitCount = count;
+            RefreshCount();
+        }
+
+        public void SetUnitPrefab(GameObject prefab)
+        {
+            unitPrefab = prefab;
+            unitImage.sprite = prefab.GetComponent<Sprite>();
+        }
+
+        public void OnUnitButtonClick()
+        {
+            buttonListeners.Invoke(unitPrefab);
+            unitCount--;
+            RefreshCount();
+            if (unitCount == 0)
+            {
+                gameObject.GetComponent<Button>().interactable = false;
+            }
+        }
+        
+        private void Awake()
+        {
+            unitImage = gameObject.transform.GetChild(0).GetComponent<Image>();
+            unitCountTxt = gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        }
+
+        private void RefreshCount()
+        {
+            unitCountTxt.text = unitCount.ToString();
+        }
+    }
+}
